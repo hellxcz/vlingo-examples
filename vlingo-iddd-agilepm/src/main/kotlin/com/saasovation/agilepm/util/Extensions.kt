a2c08505371ce5f42c3c1a3a93f173adbfadd52c
hellxcz <zdenek.heller@erstegroup.com>
2019-08-07 20:03:42 +0200

    introduced extension for registering event handler on entity

--- vlingo-iddd-agilepm/src/main/kotlin/com/saasovation/agilepm/util/Extensions.kt
+++ vlingo-iddd-agilepm/src/main/kotlin/com/saasovation/agilepm/util/Extensions.kt
@@ -1,9 +1,33 @@
 package com.saasovation.agilepm.util
 
+import io.vlingo.lattice.model.sourcing.EventSourced
+import io.vlingo.lattice.model.sourcing.Sourced
+import io.vlingo.symbio.Source
+
 val emptyString = ""
 
 val emptyInt = Int.MAX_VALUE
 
-fun String.Companion.empty () = emptyString
+fun String.Companion.empty() = emptyString
 
 fun Int.Companion.empty() = emptyInt
+
+abstract class EventSourcedEx<SNAPSHOT> : EventSourced() {
+  companion object {
+    fun <SOURCED : Sourced<*>, SOURCE : Source<*>> registerConsumerEx(
+      sourcedType: Class<SOURCED>,
+      sourceType: Class<SOURCE>,
+      consumer: SOURCED.(SOURCE) -> Unit
+    ) {
+      registerConsumer(sourcedType, sourceType, consumer)
+    }
+  }
+
+  @Suppress("UNCHECKED_CAST")
+  override fun <S> snapshot(): S {
+    return snapshotEx() as S
+  }
+
+  abstract fun snapshotEx(): SNAPSHOT?
+
+}
